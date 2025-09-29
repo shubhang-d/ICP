83e1bf4e9e76d8f567ab34c973ab502e6d9d10aa
Shubhang Dixit <shubhangd205@gmail.com>
2025-09-29 22:49:17 +0530

    rotate-image

---
+++ rotate-image.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        swap(matrix);
+    }
+    public void transpose(int[][] matrix){
+        for(int i =0; i<matrix.length;i++){
+            for(int j = i+1;j<matrix[0].length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int[] a: matrix){
+            for(int l: a){
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+    public void swap(int[][] matrix){
+        int left = 0;
+        int right = matrix[0].length-1;
+        while(left<=right){
+            for(int i = 0;i<matrix.length;i++){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+            }
+            left++;
+            right--;
+        }
+    }
+}
