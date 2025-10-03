ae5fb8f5036e3ab8e0e9e1cea035a7b62d2cbe18
Shubhang Dixit <shubhangd205@gmail.com>
2025-10-03 08:29:25 +0530

    queue-reconstruction-by-height

--- queue-reconstruction-by-height.java
+++ queue-reconstruction-by-height.java
@@ -1,26 +1,18 @@
-import java.util.Arrays;
+import java.util.*;
 
-class q{
-    public static void main(String[] args) {
-        int[][] people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
-        sol sol = new sol();
-        System.out.println(sol.reconstructQueue(people));
-    }
-}
-class sol{
-    public int[][] reconstructQueue(int[][] people){
-        Arrays.sort(people, (a,b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
-        for(int i =1;i<people.length;i++){
-            
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] == b[0]) 
+                return a[1] - b[1]; // same height → k ascending
+            return b[0] - a[0];     // height descending
+        });
+
+        List<int[]> res = new ArrayList<>();
+        for (int[] p : people) {
+            res.add(p[1], p); // insert at index k
         }
 
-
-        for(int[] a: people){
-            for(int l: a){
-                System.out.print(l + " ");
-            }
-            System.out.println();
-        }
-        return people;
+        return res.toArray(new int[people.length][]);
     }
 }
